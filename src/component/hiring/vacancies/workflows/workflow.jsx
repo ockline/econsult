e75d4a7c78d973b3b0bb23c8507e9e9e45394faa
ockline.msungu <153594520+ockline@users.noreply.github.com>
2024-08-17 16:52:30 +0300

    working on the adding comment on workflow and build file for deployment

--- src/component/hiring/vacancies/workflows/workflow.jsx
+++ src/component/hiring/vacancies/workflows/workflow.jsx
@@ -109,7 +109,94 @@ const Workflow = ({local_varaiable,ThemeChanger}) => {
 							<h5 className="box-title">Workflow Section</h5>
                         </div>
                      {roles.includes('VI') && (
-    <div className="box-body">
+                            <div className="box-body">
+                                         <div className="col-span-12 lg:col-span-6">
+					<div className="box">
+						<div className="box-header">
+							<h5 className="box-title"> Workflow History</h5>
+						</div>
+						<div className="box-body">
+							<div className="hs-accordion-group">
+								<div className="hs-accordion active overflow-hidden bg-white border -mt-px first:rounded-t-sm last:rounded-b-sm dark:bg-bgdark dark:border-white/10" id="hs-accordion-heading-1-primary">
+									<button
+										className="hs-accordion-toggle hs-accordion-active:text-secondary hs-accordion-active:bg-secondary/10 group py-4 px-5 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-start text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-secondary dark:text-gray-200 dark:hover:text-white/80"
+										aria-controls="hs-accordion-collapse-1-primary" type="button">
+                      Initiator
+										
+                                                       
+                                                        <i className= "ti ti-chevron-up hs-accordion-active:hidden hs-accordion-active:text-secondary"></i>
+										<svg
+											className="hs-accordion-active:block hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+									</button>
+									<div id="hs-accordion-collapse-1-primary"
+										className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+										aria-labelledby="hs-accordion-heading-1-primary">
+										<p className="text-gray-800 dark:text-gray-200 py-4 px-5">
+											<em>This is the third item's accordion body.</em> It is hidden by default, until the collapse
+										</p>
+									</div>
+								</div>
+								<div className="hs-accordion overflow-hidden bg-white border -mt-px first:rounded-t-sm last:rounded-b-sm dark:bg-bgdark dark:border-white/10" id="hs-accordion-heading-2-primary">
+									<button
+										className="hs-accordion-toggle hs-accordion-active:text-secondary hs-accordion-active:bg-secondary/10 group py-4 px-5 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-start text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-secondary dark:text-gray-200 dark:hover:text-white/80"
+										aria-controls="hs-accordion-collapse-1-primary" type="button">
+                    Reviewer
+										<svg
+											className="hs-accordion-active:hidden hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+										<svg
+											className="hs-accordion-active:block hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+									</button>
+									<div id="hs-accordion-collapse-2-primary"
+										className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+										aria-labelledby="hs-accordion-heading-2-primary">
+										<p className="text-gray-800 dark:text-gray-200 py-4 px-5">
+											<em>This is the third item's accordion body.</em> It is 
+										</p>
+									</div>
+								</div>
+
+								<div className="hs-accordion overflow-hidden bg-white border -mt-px first:rounded-t-sm last:rounded-b-sm dark:bg-bgdark dark:border-white/10" id="hs-accordion-heading-3-primary">
+									<button
+										className="hs-accordion-toggle hs-accordion-active:text-secondary hs-accordion-active:bg-secondary/10 group py-4 px-5 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-start text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-secondary dark:text-gray-200 dark:hover:text-white/80"
+										aria-controls="hs-accordion-collapse-1-primary" type="button">
+                      Approval
+										<svg
+											className="hs-accordion-active:hidden hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+										<svg
+											className="hs-accordion-active:block hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+									</button>
+									<div id="hs-accordion-collapse-3-primary"
+										className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+										aria-labelledby="hs-accordion-heading-3-primary">
+										<p className="text-gray-800 dark:text-gray-200 py-4 px-5">
+											<em>This is the third item's accordion body.</em> It 
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
         <form className="space-y-3">
             <label htmlFor="input-label" className="ti-form-label">Comment</label>
             <textarea
@@ -131,7 +218,94 @@ const Workflow = ({local_varaiable,ThemeChanger}) => {
 )}
 
                         {roles.includes('VR') && (
-                        <div className="box-body">
+                            <div className="box-body">
+                                         <div className="col-span-12 lg:col-span-6">
+					<div className="box">
+						<div className="box-header">
+							<h5 className="box-title"> Workflow History</h5>
+						</div>
+						<div className="box-body">
+							<div className="hs-accordion-group">
+								<div className="hs-accordion active overflow-hidden bg-white border -mt-px first:rounded-t-sm last:rounded-b-sm dark:bg-bgdark dark:border-white/10" id="hs-accordion-heading-1-primary">
+									<button
+										className="hs-accordion-toggle hs-accordion-active:text-secondary hs-accordion-active:bg-secondary/10 group py-4 px-5 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-start text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-secondary dark:text-gray-200 dark:hover:text-white/80"
+										aria-controls="hs-accordion-collapse-1-primary" type="button">
+                      Initiator
+										
+                                                       
+                                                        <i className= "ti ti-chevron-up hs-accordion-active:hidden hs-accordion-active:text-secondary"></i>
+										<svg
+											className="hs-accordion-active:block hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+									</button>
+									<div id="hs-accordion-collapse-1-primary"
+										className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+										aria-labelledby="hs-accordion-heading-1-primary">
+										<p className="text-gray-800 dark:text-gray-200 py-4 px-5">
+											<em>This is the third item's accordion body.</em> It is hidden by default, until the collapse
+										</p>
+									</div>
+								</div>
+								<div className="hs-accordion overflow-hidden bg-white border -mt-px first:rounded-t-sm last:rounded-b-sm dark:bg-bgdark dark:border-white/10" id="hs-accordion-heading-2-primary">
+									<button
+										className="hs-accordion-toggle hs-accordion-active:text-secondary hs-accordion-active:bg-secondary/10 group py-4 px-5 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-start text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-secondary dark:text-gray-200 dark:hover:text-white/80"
+										aria-controls="hs-accordion-collapse-1-primary" type="button">
+                    Reviewer
+										<svg
+											className="hs-accordion-active:hidden hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+										<svg
+											className="hs-accordion-active:block hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+									</button>
+									<div id="hs-accordion-collapse-2-primary"
+										className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+										aria-labelledby="hs-accordion-heading-2-primary">
+										<p className="text-gray-800 dark:text-gray-200 py-4 px-5">
+											<em>This is the third item's accordion body.</em> It is 
+										</p>
+									</div>
+								</div>
+
+								<div className="hs-accordion overflow-hidden bg-white border -mt-px first:rounded-t-sm last:rounded-b-sm dark:bg-bgdark dark:border-white/10" id="hs-accordion-heading-3-primary">
+									<button
+										className="hs-accordion-toggle hs-accordion-active:text-secondary hs-accordion-active:bg-secondary/10 group py-4 px-5 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-start text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-secondary dark:text-gray-200 dark:hover:text-white/80"
+										aria-controls="hs-accordion-collapse-1-primary" type="button">
+                      Approval
+										<svg
+											className="hs-accordion-active:hidden hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+										<svg
+											className="hs-accordion-active:block hs-accordion-active:text-secondary hs-accordion-active:group-hover:text-secondary hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-white/70"
+											width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+												stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
+										</svg>
+									</button>
+									<div id="hs-accordion-collapse-3-primary"
+										className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+										aria-labelledby="hs-accordion-heading-3-primary">
+										<p className="text-gray-800 dark:text-gray-200 py-4 px-5">
+											<em>This is the third item's accordion body.</em> It 
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
                              <form className = "space-y-3">
                             <label htmlFor="input-label"
                                     className = "ti-form-label">Comment</label>
