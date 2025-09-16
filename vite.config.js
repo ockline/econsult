09bd5824f697fa6cfff869321ac75164ab5aaf06
ockline.msungu <153594520+ockline@users.noreply.github.com>
2025-09-16 22:16:49 +0300

    working on handling navigation

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // 'process.env': {}
-     minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
-        cssMinify: process.env.APP_ENV === 'production',
-    'process.env.REACT_APP_API_BASE_URL': JSON.stringify('http://127.0.0.1:8000/api'),
-    'import.meta.env.VITE_REACT_APP_DOC_BASE_URL': JSON.stringify('http://127.0.0.1:8000'),
-    
-    //  'process.env.REACT_APP_API_BASE_URL': JSON.stringify('https://socratepre-api.socrate.co.tz/api'),
-    //  'import.meta.env.VITE_REACT_APP_DOC_BASE_URL': JSON.stringify('https://socratepre-api.socrate.co.tz'),
-    
-  },
   build: {
-    chunkSizeWarningLimit: 5000000,
-    minify: true,
-    },
-    server: {
-      host: true,
-       port: 5173
-    }
-    
+    chunkSizeWarningLimit: 5000000, // optional, to allow big chunks
+    minify: true,                   // always minify in production
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
 })
