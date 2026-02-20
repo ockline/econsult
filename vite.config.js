f11eae1cd9a9424705b2c5b29cded54aa75231ba
ockline.msungu <153594520+ockline@users.noreply.github.com>
2026-02-20 23:29:15 +0300

    handle file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Uncomment and set base when deploying under a subpath (e.g. XAMPP) to avoid MIME errors:
-  // base: '/core_system/econsult/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 5000000, // optional, to allow big chunks
